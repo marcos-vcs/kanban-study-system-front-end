13bc4f21aa267a6b8b0b48a7a65ec8ab45394a36
marcos <marcosviniciocardoso123@gmail.com>
2022-01-06 11:23:46 -0300

    this commit update board

--- src/app/components/boards-page/boards-page.component.ts
+++ src/app/components/boards-page/boards-page.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 interface IItemObject {
   id: number;
   name: string;
+  board: string;
+}
+interface KanbanDemand {
+  title : string;
+  description : string;
+}
+
+interface KanbanBoard {
+  id: string;
+  title: string;
+  demands : KanbanDemand[];
 }
 
 @Component({
@@ -17,20 +29,86 @@ export class BoardsPageComponent implements OnInit {
   ngOnInit(): void {
   }
   itemObjectsLeft: IItemObject[] = [
-    { id: 1, name: 'Windstorm' },
-    { id: 2, name: 'Bombasto' },
-    { id: 3, name: 'Magneta' }
+    { id: 1, name: 'Windstorm', board: 'Iniciadas' },
+    { id: 2, name: 'Bombasto' , board: 'Iniciadas' },
+    { id: 3, name: 'Magneta'  , board: 'Iniciadas' },
   ];
 
   itemObjectsRight: IItemObject[] = [
-    { id: 4, name: 'Tornado' },
-    { id: 5, name: 'Mr. O' },
-    { id: 6, name: 'Tomato' }
+    { id: 4, name: 'Tornado', board: 'Finalizadas' },
+    { id: 5, name: 'Mr. O', board: 'Finalizadas' },
+    { id: 6, name: 'Tomato', board: 'Finalizadas' },
   ];
 
+  boards: KanbanBoard[] = [
+    {
+      id: '1',
+      title: 'Iniciadas',
+      demands: [
+        {
+          title: 'Titulo da demanda',
+          description: 'Descrição da demanda'
+        },
+        {
+          title: 'Titulo da demanda',
+          description: 'Descrição da demanda'
+        }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Iniciadas',
+      demands: [
+        {
+          title: 'Titulo da demanda',
+          description: 'Descrição da demanda'
+        },
+        {
+          title: 'Titulo da demanda',
+          description: 'Descrição da demanda'
+        }
+      ]
+    },
+    {
+      id: '3',
+      title: 'Iniciadas',
+      demands: [
+        {
+          title: 'Titulo da demanda',
+          description: 'Descrição da demanda'
+        },
+        {
+          title: 'Titulo da demanda',
+          description: 'Descrição da demanda'
+        }
+      ]
+    }
+  ];
+
+  toModalObject: IItemObject = {
+    id: 0,
+    name: '',
+    board: ''
+  };
+
   openModal($event: any){
-    let event : string = $event.target;
-    console.log(event);
+    let event = $event.path[0].__ngContext__[30].initData;
+    this.toModalObject = event;
+  }
+  id: string = '';
+  name: string = '';
+
+  formSubmit(f : NgForm){
+    let newItem = {
+      id: f.value.id,
+      name: f.value.name,
+      board: ''
+    }
+
+    if(this.toModalObject.board){
+
+    }
+
 
   }
 
